701f5b3843f476d547f2e1a34ab0ba7df46c5ce3
Samantha Carly <samanthaccarly@aol.com>
2019-04-08 16:49:32 -0700

    more testing

--- impact.js
+++ impact.js
@@ -1,15 +1,17 @@
 
 
 
-function getData(){
-  var data = JSON.parse(this.response)
-  console.log(data.url)
-  alert("onload ran and exit")
-}
+
 function fireAPI(){
-  alert("enter API function call")
   var request = new XMLHttpRequest()
   request.open('GET', 'http://httpbin.org/get', true)
-  request.onload = getData()
+  alert("opened API endpoint")
   request.send()
+  alert("performed send")
+  request.onload = function(){
+    var data = JSON.parse(this.response)
+    console.log(data.url)
+    alert("onload ran and exit")
+  }
+
 }
