ef848f7f32325b83fa115494c14b311367557996
Samantha Carly <samanthaccarly@aol.com>
2019-04-08 16:33:44 -0700

    moved nested function

--- impact.js
+++ impact.js
@@ -1,13 +1,15 @@
 
 
+
+function getData(){
+  var data = JSON.parse(this.response)
+  console.log(data.url)
+  alert("onload ran and exit")
+}
 function fireAPI(){
   alert("enter API function call")
   var request = new XMLHttpRequest()
   request.open('GET', 'http://httpbin.org/get', true)
-  request.onload = function() {
-    var data = JSON.parse(this.response)
-    console.log(data.url)
-    alert("onload ran")
-  }
+  request.onload = getData()
   request.send()
 }
