80ae350001303c2fd76f997a12075c42e138b791
Samantha Carly <samanthaccarly@aol.com>
2019-04-08 16:19:52 -0700

    testing with alert

--- impact.js
+++ impact.js
@@ -1,13 +1,13 @@
 
 
 function fireAPI(){
-  alert("Hello World")
+  alert("enter API function call")
   var request = new XMLHttpRequest()
   request.open('GET', 'http://httpbin.org/get', true)
   request.onload = function() {
     var data = JSON.parse(this.response)
     console.log(data.url)
-    alert("Hello World")
+    alert("onload ran")
   }
   request.send()
 }
