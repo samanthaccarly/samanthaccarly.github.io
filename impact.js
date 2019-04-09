b441c6f654b091f0851b632f8a14e936dced4111
Samantha Carly <samanthaccarly@aol.com>
2019-04-08 17:17:58 -0700

    updating HTTP calls

--- impact.js
+++ impact.js
@@ -6,13 +6,14 @@ function fireAPI(){
   var request = new XMLHttpRequest()
   request.open('GET', 'http://httpbin.org/get', true)
   alert("opened API endpoint")
+  request.onreadystatechange = function(){
+    if (request.readystate == 4 && request.status == 200){
+      alert(request.status)
+    }
+    //var data = JSON.parse(this.response)
+    //console.log(data.url)
+    alert("statechange ran and exit")
+  }
   request.send()
   alert("performed send")
-  request.onload = function(){
-    alert("onload ran")
-    var data = JSON.parse(this.response)
-    console.log(data.url)
-    alert("onload ran and exit")
-  }
-
 }
