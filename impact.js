506d460c7cd6196addc6886cb68e67990e012a06
Samantha Carly <samanthaccarly@aol.com>
2019-04-09 17:51:15 -0700

    https for endpoint

--- impact.js
+++ impact.js
@@ -4,7 +4,7 @@
 
 function fireAPI(){
   var request = new XMLHttpRequest()
-  request.open('GET', 'http://httpbin.org/get', true)
+  request.open('GET', 'https://httpbin.org/get', true)
   alert("opened API endpoint")
   request.onreadystatechange = function(){
     if (request.readystate == 4 && request.status == 200){
