28853635fa3a8b59604962ea639c820326592177
Samantha Carly <samanthaccarly@aol.com>
2019-04-09 18:10:37 -0700

    Update impact.js

--- impact.js
+++ impact.js
@@ -9,10 +9,10 @@ function fireAPI(){
   request.onreadystatechange = function(){
     if (request.readystate == 4 && request.status == 200){
       alert(request.status)
+      var data = JSON.parse(this.response)
+      console.log(data.url)
+      alert("statechange ran and exit")
     }
-    var data = JSON.parse(this.response)
-    console.log(data.url)
-    alert("statechange ran and exit")
   }
   request.send()
   alert("performed send")
