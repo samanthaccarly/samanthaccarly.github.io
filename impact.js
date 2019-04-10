b4b6580cd57bdb56ca72163186013590083a8f29
Samantha Carly <samanthaccarly@aol.com>
2019-04-09 22:00:59 -0700

    testing if statement

--- impact.js
+++ impact.js
@@ -8,6 +8,7 @@ function fireAPI(){
   alert("opened API endpoint")
   request.onreadystatechange = function(){
     if (request.readystate == 4 && request.status == 200){
+        console.log("readystate is 4 and status is 200")
         console.log(request.responseText)
       }
       alert("statechange ran and exit")
