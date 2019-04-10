b4ecb08477c824d1754bf03ea00ee79147f14aec
Samantha Carly <samanthaccarly@aol.com>
2019-04-09 21:33:57 -0700

    update console output

--- impact.js
+++ impact.js
@@ -8,11 +8,9 @@ function fireAPI(){
   alert("opened API endpoint")
   request.onreadystatechange = function(){
     if (request.readystate == 4 && request.status == 200){
-      alert(request.status)
-      var data = JSON.parse(this.response)
-      console.log(data)
-      //alert("statechange ran and exit")
-    }
+        console.log(request.responseText)
+      }
+      alert("statechange ran and exit")
   }
   request.send()
   alert("performed send")
